0808778e4ab2513d9ee8e49951027a3028b34b20
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-08-01 22:01:50 -0300

    feat:adiciona aula 02 sobre npm

---
+++ Aula01/Aula 02/exemplo2.js
@@ -0,0 +1,14 @@
+const data = new Date()
+const dia = data.getDate()
+const mes = data.getMonth()
+const ano = data.getFullYear()
+
+console.log(`Hoje é ${dia}/${mes}/${ano}`)
+const data = new Date()
+const dia = data.getDate()
+const mes = data.getMonth() + 1
+const ano = data.getFullYear()
+
+console.log(`Hoje é ${dia}/${mes}/${ano}`)
+
+// Run this file with the command:
